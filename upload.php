9ace6d48a48afec0b5d44ab286977dec636245cc
Dcarpent <35977599+Dcarpent@users.noreply.github.com>
2023-03-09 01:55:26 -0800

    Added Manually; Forgot to pull first

--- upload.php
+++ upload.php
@@ -4,10 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
     <title>Drag, Drop & Browse</title>
     <base href=".">
+
+    <!-- FontAwesome 6 -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+   
+    <!-- Custom Css -->
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
@@ -18,21 +21,21 @@
             <div class="icon">
                 <i class="fas fa-images"></i>
             </div>
-
             <span class="header"> Drag & Drop </span>
             <span class="header"> or <span class="button"> browse </span></span>
             <span class="support">Supports: JPG, JPEG & PNG</span>
-
         </div>
     </div>   
    
     <footer >
         <a target="_blank" href="https://www.youtube.com/watch?v=HuVI3p1MNPo">Click here to open File Upload tutorial</a>
     </footer>
- 
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js" integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="css/script.js"></script>
+    <!-- JQuery -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js" integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    
+    <!-- Custom Scripts -->
+    <script src="css/script.js"></script>
 
 </body>
 </html>
